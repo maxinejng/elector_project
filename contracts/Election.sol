44587c197880cab91034d622b5489b1d13dbd3a0
maxinejng <maxinejangpro@gmail.com>
2023-01-17 00:19:50 +0100

    Beginning

---
+++ contracts/Election.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Election {
+
+    string public candidate;
+
+    //constructor
+    constructor() {
+        candidate = "candidate 1";
+    }
+}
